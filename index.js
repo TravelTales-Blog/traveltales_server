abe77e6d0b5bffecf95490098af5382d2f871726
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-04-24 00:04:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+ const routes = require('./src/routes/router');
+ 
+ require('dotenv').config()
+ 
+ const app = express();
+ app.use(express.json());
+ 
+ app.use('/api', routes);
+ 
+ const PORT = process.env.PORT || 3000;
+ app.listen(PORT, () => {
+     console.log(`Server running on port ${PORT}`);
+ });
